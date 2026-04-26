3c25a8418f6066057659c56e8601791d499b8df5
kaiichen <b14902024@csie.ntu.edu.tw>
2026-04-26 15:43:33 +0800

    Add logging system for treap :)

--- src/treap.py
+++ src/treap.py
@@ -1,6 +1,8 @@
 import random, json
 
 random.seed(48763)
+_roots=[]
+log=[]
 class node():
     def __init__(self, val:int):
         self.val=val
@@ -8,8 +10,9 @@ class node():
         # self.id=id
         self.sz=1
         self.hid=random.random()
-        self.l=None
-        self.r=None
+        self.l:node|None=None
+        self.r:node|None=None
+        self.highlight=0 # 0=default, 1=on, -1=off 
     
     def pull(self):
         self.sz=1
@@ -22,6 +25,8 @@ class node():
             self.sz += self.r.sz
 
 def merge(a:node, b:node):
+    global log, _roots
+    log.append({"name":"merge","data":print_treap(_roots, [a,b])})
     if not a or not b:
         return a or b
     if a.hid > b.hid:
@@ -35,34 +40,65 @@ def merge(a:node, b:node):
         return b
 
 # left part has size = k
+# highlight left side
 def spilt(cur: node, k:int): # -> a, b
+    global log, _roots
+    log.append({"name":"spilt","data":print_treap(_roots, [cur])})
     if not cur:
         return None, None
+
+    if k == 0: 
+        return None, cur
+    
     # no push
     lsz = cur.l.sz if cur.l else 0
     if lsz >= k:
+        cur.highlight=-1
+        if cur.l: cur.l.highlight=1
+        log.append({"name":"spilt","data":print_treap(_roots, [cur])})
+
         a, cur.l = spilt(cur.l, k)
         cur.pull()
         return a, cur
     else:
+        cur.highlight=1
+        if cur.r: cur.r.highlight=-1
+        log.append({"name":"spilt","data":print_treap(_roots, [cur])})
+
         cur.r, b = spilt(cur.r, k - lsz - 1)
         cur.pull()
+
         return cur, b
 
-def print_treap(root):
-    def build_dict(node):
+def reset_hili(node:node):
+    if not node: return
+    node.highlight=0
+    reset_hili(node.l)
+    reset_hili(node.r)
+
+def print_treap(roots, cur=None):
+    def build_dict(node:node, prvh, cur):
         if not node:
             return None
-        
+
         ret= {
             "node_id": str(id(node)),  # 利用 id() 產生唯一標識符
             # "node_id": node.id,
             "val": node.val,
             "priority": round(node.hid, 6),  # 取小數點後 6 位讓版面更簡潔
-            "range_max": node.mx,
-            "left": build_dict(node.l),
-            "right": build_dict(node.r)
+            "range_max": node.mx
         }
+
+        if node.highlight==1 or (node.highlight==0 and prvh==1):
+            ret["highlight1"]=True
+            prvh=1
+        else: prvh=-1
+
+        if cur and node in cur: ret["highlight2"]=True
+
+        ret["left"]=build_dict(node.l, prvh, cur)
+        ret["right"]=build_dict(node.r, prvh, cur)
+
         if not node.l and node.r:
             ret["left"]={
                 "node_id": ret["node_id"]+"_null",
@@ -80,50 +116,96 @@ def print_treap(root):
         "isVirtual": True,
         "node_id": "vroot",
         "children":[
-            build_dict(root)
+            build_dict(root, -1, cur) for root in roots if root
         ]
     }
-    # 建立字典結構並轉換為縮排好的 JSON 字串
-    return json.dumps(vroot, indent=4)
+    return vroot
+
+def spilt_with_log(cur, k):
+    global _roots, log
+    a,b=spilt(cur,k)
+    _roots.pop()
+    _roots.append(a)
+    _roots.append(b)
+    log.append({"name":"finish spilt","data":print_treap(_roots)})
+    reset_hili(a)
+    reset_hili(b)
+    # log.append({"name":"finish spilt","data":print_treap(_roots)})
+    return a,b
+
+def merge_with_log(a,b):
+    global _roots, log
+    if a: a.highlight=1
+    ret=merge(a,b)
+    _roots.pop()
+    _roots.pop()
+    _roots.append(ret)
+    log.append({"name":"finish merge","data":print_treap(_roots)})
+    reset_hili(ret)
+    return ret
 
 class Treap():
+    global _roots, log
     def __init__(self):
         self.root : node | None =None
+        _roots.append(self.root)
 
-    def query(self, l, r): # root will not change
+    def query(self, l, r): # root will not change, -> int
         if not self.root: raise ValueError("Tree is empty")
         if l > r: raise ValueError("l must be <= r")
         if l <= 0 or r > self.root.sz: raise ValueError("Index out of bounds")
-        a, b= spilt(self.root, l-1)
-        b, c= spilt(b, r-l+1)
+        a, b= spilt_with_log(self.root, l-1)
+        b, c= spilt_with_log(b, r-l+1)
         ans = b.mx
-        merge(a, merge(b,c))
+        merge_with_log(a, merge_with_log(b,c))
         return ans
 
-    def insert(self, k, val): # -> new root
+    def insert(self, k, val): # -> void
         current_sz = self.root.sz if self.root else 0
         if k < 0 or k > current_sz: raise ValueError("Index out of bounds")
-        a,b=spilt(self.root, k)
+        a,b=spilt_with_log(self.root, k)
         newnode=node(val)
-        self.root = merge(a, merge(newnode, b))
+        tmp=_roots.pop()
+        _roots.append(newnode)
+        _roots.append(tmp)
+        self.root = merge_with_log(a, merge_with_log(newnode, b))
 
-    def remove(self, k):
+    def remove(self, k): # -> void
         if not self.root: raise ValueError("Tree is empty")
         if k <= 0 or k > self.root.sz: raise ValueError("Index out of bounds")
-        a,b= spilt(self.root, k-1)
-        b,c= spilt(b,1)
-        self.root= merge(a,c)
+        a,b= spilt_with_log(self.root, k-1)
+        b,c= spilt_with_log(b,1)
+        _roots.pop()
+        _roots.pop()
+        _roots.append(c)
+        log.append({"name":"delete node","data":print_treap(_roots)})
+        self.root= merge_with_log(a,c)
     
-    def build(self, nodes): # nodes[i]=val
+    def build(self, nodes): # nodes[i]=val, -> void
+        global _roots,log
         for i in nodes:
             self.root=merge(self.root, node(i))
+        _roots.pop()
+        _roots.append(self.root)
+        log.clear()
+
+
+def flush_log():
+    global log
+    # return log
+    # print(json.dumps(log, indent=4)) # for debugging
+    res, log = log, [] # for !debug
+    return res # for !debug
+
 
 if __name__=='__main__':
     t = Treap()
     t.build([4,8,7,6,3])
-    print(print_treap(t.root))
-    t.remove(2)
-    t.remove(3)
-    t.insert(0,100)
-    print(t.query(2,4))
-    print(print_treap(t.root))
+    # t.insert(2,100)
+    # flush_log()
+    # print("--------------------------")
+    # t.remove(2)
+    # flush_log()
+    # print("--------------------------")
+    t.query(3,5)
+    print(flush_log())
